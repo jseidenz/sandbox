e348d16a7ecd94f025f00338df72a4354fa48285
jseidenz <jseidenz@gmail.com>
2020-12-05 19:24:50 -0800

    Terrain buyoancy and all sorts of things.

---
+++ Assets/Scripts/Buoyancy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Threading.Tasks;
+
+public class Buoyancy : MonoBehaviour 
+{
+    [SerializeField] float m_amount;
+    [SerializeField] float m_rate;
+
+    float m_original_y;
+
+    void Start()
+    {
+        m_original_y = transform.position.y;
+    }
+
+    void Update()
+    {
+        var pos = transform.position;
+        pos.y = m_original_y + Mathf.Sin(Time.time * m_rate) * m_amount;
+        transform.position = pos;
+    }
+}
