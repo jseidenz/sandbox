bfb731be5e2395de83d159d099ae2838499e233b
jseidenz <jseidenz@gmail.com>
2020-12-07 13:47:10 -0800

    Branches for debugging.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -131,11 +131,17 @@ public class Game : MonoBehaviour
     {
         m_dirty_chunk_ids.Clear();
         m_solid_simulation.Update(m_dirty_chunk_ids);
-        m_solid_mesher.Triangulate(m_dirty_chunk_ids);
+        if (m_dirty_chunk_ids.Count > 0)
+        {
+            m_solid_mesher.Triangulate(m_dirty_chunk_ids);
+        }
 
         m_dirty_chunk_ids.Clear();
         m_liquid_simulation.Update(m_dirty_chunk_ids);
-        m_liquid_mesher.Triangulate(m_dirty_chunk_ids);
+        if (m_dirty_chunk_ids.Count > 0)
+        {
+            m_liquid_mesher.Triangulate(m_dirty_chunk_ids);
+        }
     }
 
     void CreateGroundPlane()
