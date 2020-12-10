c2c07c6f840f0f115b7540899cc1fbf6bcdd9421
jseidenz <jseidenz@gmail.com>
2020-12-09 19:28:26 -0800

    More island work.

--- Assets/Scripts/UI/CreateIslandScreen.cs
+++ Assets/Scripts/UI/CreateIslandScreen.cs
@@ -8,12 +8,47 @@ public class CreateIslandScreen : MonoBehaviour
     [SerializeField] Button m_create_button;
     [SerializeField] Button m_back_button;
     [SerializeField] TMPro.TMP_InputField m_island_name;
+    [SerializeField] TMPro.TMP_InputField m_your_name;
 
     bool m_is_screen_faded;
 
+    string[] m_pronouns = new string[]
+    {
+        "The",
+        "Our",
+    };
+
+    string[] m_adjectives = new string[]
+    {
+        "Magical",
+        "Lovely",
+        "Enchanting",
+        "Delightful",
+        "Good",
+        "Exotic"
+    };
+
+    string[] m_nouns = new string[]
+    {
+        "Hills",
+        "Land",
+        "Island",
+        "Archipelago",
+        "Retreat",
+        "Isle",
+        "Reef",
+        "Peninsula",
+        "Cove",
+        "Delight",
+        "Bliss",
+        "Home",
+        "Paradise",
+        "Place"
+    };
+
     void Awake()
     {
-        m_island_name.text = "Paradise Hills";
+        m_your_name.text = "Islander";
 
         m_back_button.onClick.AddListener(() =>
         {
@@ -35,6 +70,18 @@ public class CreateIslandScreen : MonoBehaviour
 
     void OnEnable()
     {
+        var noun = m_nouns[UnityEngine.Random.Range(0, m_nouns.Length)];
+        var adjective = m_adjectives[UnityEngine.Random.Range(0, m_adjectives.Length)];
+
+        var island_name = $"{adjective} {noun}";
+        if (UnityEngine.Random.value > 0.25f)
+        {
+            var pronoun = m_pronouns[UnityEngine.Random.Range(0, m_pronouns.Length)];
+            island_name = $"{pronoun} {island_name}";
+        }
+
+        m_island_name.text = island_name;
+
         m_is_screen_faded = false;
     }
 
