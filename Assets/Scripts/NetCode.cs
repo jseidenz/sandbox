d58df364e82c0285ec1114ebc20a84e2ca2df777
jseidenz <jseidenz@gmail.com>
2020-12-09 16:40:16 -0800

    Netcode now connects.

---
+++ Assets/Scripts/NetCode.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetCode : MonoBehaviourPunCallbacks
+{
+    static string VERSION_NUMBER = "1";
+#if UNITY_EDITOR
+    static string GAME_VERSION = $"UNITY_EDITOR_{VERSION_NUMBER}";
+#else
+    static string GAME_VERSION = $"UNITY_BUILD_{VERSION_NUMBER}";
+#endif
+
+    bool m_has_joined_room;
+
+    public static NetCode Instance;
+
+    void Awake()
+    {
+        Instance = this;
+
+        Debug.Log("Connecting");
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = GAME_VERSION;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //Debug.Log("ConnectedToMaster");
+        //PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short return_code, string message)
+    {
+        Debug.Log("JoinRoomFailed");
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+
+    public override void OnCreateRoomFailed(short return_code, string message)
+    {
+        Debug.LogError($"CreateRoomFailed: {message} return_code={return_code}");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("JoinedRoom");
+        m_has_joined_room = true;
+    }
+
+    public bool HasJoinedRoom()
+    {
+        return m_has_joined_room;
+    }
+}
