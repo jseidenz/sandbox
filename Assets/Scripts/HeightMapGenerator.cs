06d2b5cb08fb4278ecf3368b109cb75544f9dca9
jseidenz <jseidenz@gmail.com>
2020-12-20 12:29:46 -0800

    Delete comments.

--- Assets/Scripts/HeightMapGenerator.cs
+++ Assets/Scripts/HeightMapGenerator.cs
@@ -7,7 +7,6 @@ public class HeightMapGenerator
         var moisture_seed = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
 
         m_elevation_noise = new OpenSimplexNoise(elevation_seed);
-        m_moisture_noise = new OpenSimplexNoise(moisture_seed);
     }
 
     public float[] GenerateHeightMap(int width_in_cells, int height_in_cells, double scale)
@@ -49,39 +48,5 @@ public class HeightMapGenerator
         return m_elevation_noise.Evaluate(x, y) / 2 + 0.5f;
     }
 
-    public float MoistureNoise(float x, float y)
-    {
-        return m_moisture_noise.Evaluate(x, y);
-    }
-
     OpenSimplexNoise m_elevation_noise;
-    OpenSimplexNoise m_moisture_noise;
-
-    /*
-        var rng1 = PM_PRNG.create(seed1);
-        var rng2 = PM_PRNG.create(seed2);
-        var gen1 = new SimplexNoise(rng1.nextDouble.bind(rng1));
-        var gen2 = new SimplexNoise(rng2.nextDouble.bind(rng2));
-        function noise1(nx, ny) { return gen1.noise2D(nx, ny) / 2 + 0.5; }
-        function noise2(nx, ny) { return gen2.noise2D(nx, ny) / 2 + 0.5; }
-
-    for (var y = 0; y<height; y++) {
-      for (var x = 0; x<width; x++) {      
-        var nx = x / width - 0.5, ny = y / height - 0.5;
-        var e = (1.00 * noise1(1 * nx, 1 * ny)
-               + 0.50 * noise1(2 * nx, 2 * ny)
-               + 0.25 * noise1(4 * nx, 4 * ny)
-               + 0.13 * noise1(8 * nx, 8 * ny)
-               + 0.06 * noise1(16 * nx, 16 * ny)
-               + 0.03 * noise1(32 * nx, 32 * ny));
-        e /= (1.00+0.50+0.25+0.13+0.06+0.03);
-        e = Math.pow(e, 5.00);
-        var m = (1.00 * noise2(1 * nx, 1 * ny)
-               + 0.75 * noise2(2 * nx, 2 * ny)
-               + 0.33 * noise2(4 * nx, 4 * ny)
-               + 0.33 * noise2(8 * nx, 8 * ny)
-               + 0.33 * noise2(16 * nx, 16 * ny)
-               + 0.50 * noise2(32 * nx, 32 * ny));
-        m /= (1.00+0.75+0.33+0.33+0.33+0.50);
-    */
 }
