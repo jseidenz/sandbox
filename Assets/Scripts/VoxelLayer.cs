b11b7cf85c68388dd94c190a504334328f5acdd4
jseidenz <jseidenz@gmail.com>
2020-12-20 19:00:14 -0800

    Fix issue with updating chunks on corners

--- Assets/Scripts/VoxelLayer.cs
+++ Assets/Scripts/VoxelLayer.cs
@@ -125,10 +125,28 @@ public class VoxelLayer
                 if(dirty_occlusion_regions.HasFlag(VoxelChunk.DirtyOcclusionRegion.Left))
                 {
                     m_scratch_dirty_chunk_ids.Add(chunk_id + new Vector3Int(-1, -1, 0));
+
+                    if(dirty_occlusion_regions.HasFlag(VoxelChunk.DirtyOcclusionRegion.Near))
+                    {
+                        m_scratch_dirty_chunk_ids.Add(chunk_id + new Vector3Int(-1, -1, -1));
+                    }
+                    if (dirty_occlusion_regions.HasFlag(VoxelChunk.DirtyOcclusionRegion.Far))
+                    {
+                        m_scratch_dirty_chunk_ids.Add(chunk_id + new Vector3Int(-1, -1, 1));
+                    }
                 }
                 if (dirty_occlusion_regions.HasFlag(VoxelChunk.DirtyOcclusionRegion.Right))
                 {
                     m_scratch_dirty_chunk_ids.Add(chunk_id + new Vector3Int(1, -1, 0));
+
+                    if (dirty_occlusion_regions.HasFlag(VoxelChunk.DirtyOcclusionRegion.Near))
+                    {
+                        m_scratch_dirty_chunk_ids.Add(chunk_id + new Vector3Int(1, -1, -1));
+                    }
+                    if (dirty_occlusion_regions.HasFlag(VoxelChunk.DirtyOcclusionRegion.Far))
+                    {
+                        m_scratch_dirty_chunk_ids.Add(chunk_id + new Vector3Int(1, -1, 1));
+                    }
                 }
                 if (dirty_occlusion_regions.HasFlag(VoxelChunk.DirtyOcclusionRegion.Near))
                 {
