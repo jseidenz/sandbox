b741267f96f9e150f6c1b0f4d2235360cd7f8509
jseidenz <jseidenz@gmail.com>
2020-12-21 21:49:46 -0800

    Get rid of useless sphere cast.

--- Assets/IL3DN/Scripts/IL3DN_SimpleFPSController.cs
+++ Assets/IL3DN/Scripts/IL3DN_SimpleFPSController.cs
@@ -172,12 +172,6 @@ namespace IL3DN
             // always move along the camera forward as it is the direction that it being aimed at
             Vector3 desiredMove = transform.forward * m_Input.y + transform.right * m_Input.x;
 
-            // get a normal for the surface that is being touched to move along it
-            RaycastHit hitInfo;
-            Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
-                               m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
-            desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
-
             m_MoveDir.x = desiredMove.x * speed;
             m_MoveDir.z = desiredMove.z * speed;
 
