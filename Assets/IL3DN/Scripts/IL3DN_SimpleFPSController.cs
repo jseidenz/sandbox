26213593e02142835dd482ea92e1759cf50943c2
jseidenz <jseidenz@gmail.com>
2020-12-23 00:28:36 -0800

    Tune underwater speed.

--- Assets/IL3DN/Scripts/IL3DN_SimpleFPSController.cs
+++ Assets/IL3DN/Scripts/IL3DN_SimpleFPSController.cs
@@ -337,10 +337,25 @@ namespace IL3DN
 #if !MOBILE_INPUT
             // On standalone builds, walk/run speed is modified by a key press.
             // keep track of whether or not the character is walking or running
-            m_IsWalking = m_is_underwater || !Input.GetKey(KeyCode.LeftShift);
+            m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
 #endif
-            // set the desired speed to be walking or running
-            speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;
+            if (m_is_underwater)
+            {
+                if (!m_IsWalking)
+                {
+                    speed = m_RunSpeed * 0.66f;
+                }
+                else
+                {
+                    speed = m_WalkSpeed;
+                }
+                m_IsWalking = true;
+            }
+            else
+            {
+                speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;
+            }
+
             m_Input = new Vector2(horizontal, vertical);
 
             // normalize input if it exceeds 1 in combined length:
